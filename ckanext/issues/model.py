325d7300a3e53a900032c6b5082c92228d75b562
Rufus Pollock <rufus.pollock@okfn.org>
2014-01-22 13:22:04 +0000

    [model,bugfix][xs]: import sqlalchemy stuff from sqlalchemy rather than ckan.model.meta (which no longer has the stuff).

--- ckanext/issues/model.py
+++ ckanext/issues/model.py
@@ -2,11 +2,11 @@
 CKAN Issue Extension Data Model
 """
 import sqlalchemy as sa
+from sqlalchemy import types, Table, ForeignKey, Column, DateTime
 from sqlalchemy.sql.expression import or_
 from sqlalchemy.orm import relation, backref
 from ckan import model
 from ckan.model import meta, User, Package, Session, Resource, Group
-from ckan.model.meta import types, Table, ForeignKey, DateTime
 from ckan.model.types import make_uuid
 from datetime import datetime
 
@@ -21,12 +21,12 @@ DEFAULT_CATEGORIES = {u"broken-resource-link": "Broken resource link",
 # ------------------------------------------------------------------------------
 
 issue_category_table = Table('issue_category', meta.metadata,
-    meta.Column('id', types.Integer, primary_key = True,
+    Column('id', types.Integer, primary_key = True,
                 autoincrement = True),
-    meta.Column('name', types.Unicode(ISSUE_CATEGORY_NAME_MAX_LENGTH),
+    Column('name', types.Unicode(ISSUE_CATEGORY_NAME_MAX_LENGTH),
                 nullable=False, unique=True),
-    meta.Column('description', types.Unicode, nullable=False, unique=False),
-    meta.Column('created', DateTime, default = datetime.now, nullable = False))
+    Column('description', types.Unicode, nullable=False, unique=False),
+    Column('created', types.DateTime, default = datetime.now, nullable = False))
 
 class IssueCategory(object):
     """A Issue Category Object"""
@@ -59,26 +59,26 @@ class IssueCategory(object):
 # ------------------------------------------------------------------------------
 
 issue_table = Table('issue', meta.metadata,
-    meta.Column('id', types.Integer, primary_key = True,
+    Column('id', types.Integer, primary_key = True,
                 autoincrement = True),
-    meta.Column('issue_category_id', types.Integer,
+    Column('issue_category_id', types.Integer,
                 ForeignKey('issue_category.id', onupdate = 'CASCADE', ondelete = 'CASCADE'),
                 nullable = False),
-    meta.Column('package_id', types.UnicodeText,
+    Column('package_id', types.UnicodeText,
                 ForeignKey('package.id', onupdate = 'CASCADE', ondelete = 'CASCADE'),
                 nullable = True),
-    meta.Column('resource_id', types.UnicodeText,
+    Column('resource_id', types.UnicodeText,
                 ForeignKey('resource.id', onupdate = 'CASCADE', ondelete = 'CASCADE'),
                 nullable = True),
-    meta.Column('description', types.UnicodeText, nullable = False),
-    meta.Column('creator', types.UnicodeText,
+    Column('description', types.UnicodeText, nullable = False),
+    Column('creator', types.UnicodeText,
                 ForeignKey('user.id', onupdate='CASCADE', ondelete='SET NULL'),
                 nullable = False),
-    meta.Column('resolver', types.UnicodeText,
+    Column('resolver', types.UnicodeText,
                 ForeignKey('user.id', onupdate='CASCADE', ondelete='SET NULL'),
                 nullable = True),
-    meta.Column('resolved', DateTime),
-    meta.Column('created', DateTime, default = datetime.now, nullable = False))
+    Column('resolved', types.DateTime),
+    Column('created', types.DateTime, default = datetime.now, nullable = False))
 
 class Issue(object):
     """A Issue Object"""
@@ -122,16 +122,16 @@ meta.mapper(IssueCategory, issue_category_table)
 # ------------------------------------------------------------------------------
 
 issue_comment_table = Table('issue_comment', meta.metadata,
-    meta.Column('id', types.Integer, primary_key = True,
+    Column('id', types.Integer, primary_key = True,
                 autoincrement = True),
-    meta.Column('comment', types.Unicode, nullable=False, unique=False),
-    meta.Column('author_id', types.Unicode,
+    Column('comment', types.Unicode, nullable=False, unique=False),
+    Column('author_id', types.Unicode,
                 ForeignKey('user.id', onupdate = 'CASCADE', ondelete = 'CASCADE'),
                 nullable=False, unique=False),
-    meta.Column('issue_id', types.Integer,
+    Column('issue_id', types.Integer,
                 ForeignKey('issue.id', onupdate = 'CASCADE', ondelete = 'CASCADE'),
                 nullable=False, unique=False, index=True),
-    meta.Column('created', DateTime, default = datetime.now, nullable = False))
+    Column('created', types.DateTime, default = datetime.now, nullable = False))
 
 class IssueComment(object):
     """A Issue Comment Object"""
