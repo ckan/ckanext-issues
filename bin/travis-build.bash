57ed82b8f0d8a977c82656b38b692bf80e1703d3
joetsoi <joe.yeung.tsoi@gmail.com>
2015-04-20 11:37:43 +0100

    add travis builds for 2.2, 2.3 and master

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -10,7 +10,13 @@ sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-git checkout master
+if [ $CKANVERSION == '2.3' ]
+then
+    git checkout release-v2.3
+elif [ $CKANVERSION == '2.2' ] 
+then
+    git checkout release-v2.2.3
+fi
 python setup.py develop
 cp ./ckan/public/base/css/main.css ./ckan/public/base/css/main.debug.css
 pip install -r requirements.txt --allow-all-external
