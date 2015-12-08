233a23dbcbe9fc4fc306488e8a726e7cd26c9d2c
Tom Mortimer-Jones <tom@morty.co.uk>
2015-12-08 13:46:05 +0000

    Move code to get site_title into helper function

--- ckanext/issues/lib/helpers.py
+++ ckanext/issues/lib/helpers.py
@@ -185,6 +185,17 @@ def issues_users_who_reported_issue(abuse_reports):
     return users
 
 
+def get_site_title():
+    # older ckans
+    site_title = config.get('ckan.site_title')
+    try:
+        # from ckan 2.4
+        from ckan.model.system_info import get_system_info
+        return get_system_info('ckan.site_title', site_title)
+    except ImportError:
+        return site_title
+
+
 def get_issue_subject(issue):
     dataset = model.Package.get(issue['dataset_id'])
     return toolkit._(
