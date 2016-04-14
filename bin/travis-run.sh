b75980b6f6277400fb787ca313192f51067b470f
Ross Jones <ross@servercode.co.uk>
2016-04-14 11:40:45 +0100

    Tweak travis run script in an attempt to get solr to run

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -2,5 +2,5 @@
 
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
-sudo service jetty restart
+sudo /etc/init.d/jetty start
 nosetests --ckan --nologcapture --with-pylons=subdir/test.ini --reset-db --with-coverage --cover-package=ckanext.issues --cover-inclusive --cover-erase --cover-tests
