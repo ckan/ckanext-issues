657fe5a4d512d8eb90d1876543f1285f12877b91
Ross Jones <ross@servercode.co.uk>
2016-04-15 13:58:11 +0100

    Give jetty a chance to start

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -4,4 +4,5 @@ echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\n" | sudo tee /etc/defau
 cat /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo /etc/init.d/jetty start
+sleep 20 
 nosetests --ckan --nologcapture --with-pylons=subdir/test.ini --reset-db --with-coverage --cover-package=ckanext.issues --cover-inclusive --cover-erase --cover-tests
