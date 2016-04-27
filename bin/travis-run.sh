ba0b8e8178ab4373e065debd70725e1bfc14f74a
David Read <david.read@hackneyworkshop.com>
2016-04-27 11:56:06 +0100

    Trying to find solr home

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -e
+set -x
 
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+ls /usr/share/solr/
+pwd
+ls
 ckan/bin/solr_init/create_core.sh
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
