f5e52a167a735ca64f9f36af8640dba41230c7ee
David Read <david.read@hackneyworkshop.com>
2016-04-27 11:40:55 +0100

    Add the new core to travis.

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+ckan/bin/solr_init/create_core.sh
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 nosetests --ckan --nologcapture --with-pylons=subdir/test.ini --reset-db --with-coverage --cover-package=ckanext.issues --cover-inclusive --cover-erase --cover-tests
