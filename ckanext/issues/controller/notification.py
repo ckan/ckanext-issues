12a294d1a8fa13f7515c33c54482ce7ac4b2f3bf
Alex Corbi <a.corbi@gmail.com>
2016-05-11 17:49:45 +0200

    Fixes #20

--- ckanext/issues/controller/notification.py
+++ ckanext/issues/controller/notification.py
@@ -16,76 +16,79 @@ role_mapper ={'Admin':'admin','Editor':'reader'}
 
 def notify_create_reopen(context,issue):
 
-    notify(context,issue,"issues/email/issue_create_reopen.txt")
+  notify(context,issue,"issues/email/issue_create_reopen.txt")
 
 def notify_close(context,issue):
 
-    notify(context,issue,"issues/email/issue_close.txt")
+  notify(context,issue,"issues/email/issue_close.txt")
 
 def notify_delete(context,issue):
 
-    notify(context,issue,"issues/email/issue_delete.txt")
+  notify(context,issue,"issues/email/issue_delete.txt")
 
 
 def notify(context,issue,email_template):
 
-    notify_admin = h.asbool(config.get("ckanext.issues.notify_admin", False))
-    notify_owner = h.asbool(config.get("ckanext.issues.notify_owner", False))
-    if not notify_admin and not notify_owner:
-        return
+  notify_admin = h.asbool(config.get("ckanext.issues.notify_admin", False))
+  notify_owner = h.asbool(config.get("ckanext.issues.notify_owner", False))
+  if not notify_admin and not notify_owner:
+      return
 
-    user_obj = model.User.get(issue.user_id)
-    dataset = model.Package.get(issue.dataset_id)
+  user_obj = model.User.get(issue.user_id)
+  dataset = model.Package.get(issue.dataset_id)
 
-    extra_vars = {
-        'issue': issue,
-        'title': dataset.title,
-        'username': user_obj.name,
-        'email': user_obj.email,
-        'site_url': h.url_for(
-            controller='ckanext.issues.controller:IssueController',
-            action='show',
-            id=issue.id,
-            package_id=issue.dataset_id,
-            qualified=True
-        )
-    }
+  extra_vars = {
+      'issue': issue,
+      'title': dataset.title,
+      'username': user_obj.name,
+      'email': user_obj.email,
+      'site_url': h.url_for(
+          controller='ckanext.issues.controller:IssueController',
+          action='show',
+          id=issue.id,
+          package_id=issue.dataset_id,
+          qualified=True
+      )
+  }
 
-    if notify_owner:
-        contact_name = dataset.author or dataset.maintainer
-        contact_email =  dataset.author_email or dataset.maintainer_email
+  if notify_owner:
+    contact_name = dataset.author or dataset.maintainer
+    contact_email =  dataset.author_email or dataset.maintainer_email
 
-        email_msg = render(email_template,extra_vars=extra_vars,loader_class=NewTextTemplate)
-        send_email(contact_name,contact_email,email_msg)
+    email_msg = render(email_template,extra_vars=extra_vars,loader_class=NewTextTemplate)
+    send_email(contact_name,contact_email,email_msg)
 
-    if notify_admin:
+  if notify_admin:
 
-        # retrieve organization's admins (or the role specified on ckanext.issues.minimun_role_required) to notify
-        user_roles = action.get.roles_show(context,data_dict={'domain_object':dataset.owner_org})
-        minimun_role_required = config.get("ckanext.issues.minimun_role_required", "Anonymous")
+      # retrieve organization's admins (or the role specified on ckanext.issues.minimun_role_required) to notify
+      organization = action.get.organization_show(context,data_dict={'id':dataset.owner_org})
+      minimun_role_required = config.get("ckanext.issues.minimun_role_required", "Anonymous")
 
-        for user in user_roles['roles']:
-            if user['role'] == role_mapper[minimun_role_required]:
+      for user in organization['users']:
 
-                admin_user = model.User.get(user['user_id'])
-                admin_name = admin_user.name
-                admin_email = admin_user.email
+        if user['capacity'] == role_mapper[minimun_role_required] and user['activity_streams_email_notifications']:
 
-                email_msg = render(email_template,extra_vars=extra_vars,loader_class=NewTextTemplate)
-                send_email(admin_name,admin_email,email_msg)
+          admin_user = model.User.get(user['id'])
+          admin_name = admin_user.name
+          admin_email = admin_user.email
+
+          if admin_email != contact_email:
+            email_msg = render(email_template,extra_vars=extra_vars,loader_class=NewTextTemplate)
+            send_email(admin_name,admin_email,email_msg)
 
 def send_email(contact_name,contact_email,email_msg):
 
-    log.debug("send_email to %s %s",contact_name,contact_email)
+  log.debug("send_email to %s %s",contact_name,contact_email)
 
-    headers = {}
-    # if cc_email:
-    #     headers['CC'] = cc_email
+  headers = {}
+  # if cc_email:
+  #     headers['CC'] = cc_email
 
-    try:
+  try:
 
-        mail_recipient(contact_name, contact_email,"Dataset issue",email_msg, headers=headers)
+    mail_recipient(contact_name, contact_email,"Dataset issue",email_msg, headers=headers)
 
-    except Exception:
-        traceback.print_exc()
-        log.error('Failed to send an email message for issue notification')
+  except Exception:
+
+    traceback.print_exc()
+    log.error('Failed to send an email message for issue notification')
