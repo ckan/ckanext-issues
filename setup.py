f14848f0815f91515dc8bc0b21529fd740ae2c7a
Oleg Serbokryl <chezar1995@gmail.com>
2018-03-23 15:24:52 +0200

    add translations (#2)
    * add translations
    
    * updated plugin

--- setup.py
+++ setup.py
@@ -30,5 +30,20 @@ setup(
 
     [paste.paster_command]
     issues = ckanext.issues.commands:Issues
+    
+    [babel.extractors]
+    ckan = ckan.lib.extract:extract_ckan
 	""",
+    # If you are changing from the default layout of your extension, you may
+    # have to change the message extractors, you can read more about babel
+    # message extraction at
+    # http://babel.pocoo.org/docs/messages/#extraction-method-mapping-and-configuration
+    message_extractors={
+        'ckanext': [
+            ('**.py', 'python', None),
+            ('**.js', 'javascript', None),
+            ('**/templates/**.html', 'ckan', None),
+        ],
+    }
 )
+
